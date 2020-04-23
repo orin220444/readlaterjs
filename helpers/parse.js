f222f72f36204b71d8ca1bc63f2c4332533f4e6e
Orin Serimon <orin220444@protonmail.com>
2020-04-23 17:26:09 +0000

    bug fixes

--- helpers/parse.js
+++ helpers/parse.js
@@ -1,8 +1,15 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 async function parse(url){
+console.log("test")
     axios.get(url).then(function(response){
-        const $ = cheerio.load(response)
+        //console.log(response)
+const $ = cheerio.load(response)
+body = $("body").text()
+console.log(body)
     })
+.catch(funtion(error){
+console.log("error:", error)
+})
 }
 module.exports = parse
