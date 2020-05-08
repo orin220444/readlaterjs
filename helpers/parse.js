631c7758d026417a5cbd5429f43162004aa4e701
Orin Serimon <orin220444@protonmail.com>
2020-05-08 10:34:08 +0700

    fix formatting

--- helpers/parse.js
+++ helpers/parse.js
@@ -1,11 +1,11 @@
-const Mercury = require('@postlight/mercury-parser')
+const Mercury = require('@postlight/mercury-parser');
 /**
 * parses web page to text
 * @param {string} url url of web page to parse
 */
 async function parse(url) {
-Mercury.parse(url,{contentType: "markdown"}).then(result => {
-console.log(result.content)
-})
+  Mercury.parse(url, {contentType: 'markdown'}).then((result) => {
+    console.log(result.content);
+  });
 }
 module.exports = parse;
