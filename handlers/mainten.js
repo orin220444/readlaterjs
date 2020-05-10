96922f6e2a677d919c03019748c79aba5a6e5550
Orin Serimon <orin220444@protonmail.com>
2020-05-10 08:45:52 +0000

    some bugfixes

--- handlers/mainten.js
+++ handlers/mainten.js
@@ -7,27 +7,34 @@ module.exports = async () => {
     const post = posts[i];
     // console.log(post);
     console.log(`checking for duplicates for post ${post.originalURL}`);
-    await checkForDuplicates(post);
-  }
+try {    await checkForDuplicates(post);
+}catch (error){
+console.log(error)
+}  
+}
 };
 /**
  * checks for the duplicates because of shotten urls and redirect urls
  * @param {object} post post to check
  */
 async function checkForDuplicates(post) {
-  const realUrl = await getRealUrl(post.originalURL);
-  if (realUrl !== undefined) {
+const realUrl = setTimeout(async() => { const realurl = await getRealUrl(post.originalURL);
+return realurl
+  }, 15*1000)
+if (realUrl !== undefined) {
     if (realUrl !== post.originalURL) {
       const original = Post.findOne({originalURL: realUrl});
       if (original !== undefined) {
         if (original === post) {
           console.log('finded a duplicate!');
-          await Post.findOneAndDelete({originalURL: post.originalURL});
-          console.log('duplicate is deleted!');
+setTimeout(async(post) => {          await Post.findOneAndDelete({originalURL: post.originalURL});
+          }, 10* 1000)
+console.log('duplicate is deleted!');
         } else {
           console.log('updating url');
-          post.originalURL = await realUrl;
+setTimeout( async(post,realUrl) => {          post.originalURL = await realUrl;
           await post.save();
+}, 15, 1000)
         }
       }
     }
