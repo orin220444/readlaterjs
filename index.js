1530db23d31dc3d9d1da6452e3fc2a51a3fe1b07
Orin Serimon <orin220444@protonmail.com>
2020-05-12 16:52:15 +0700

    bot now saves cache of the db after starting

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const {
   handleAdd,
   handleQuery,
 } = require('./handlers');
+const updateLocaldb = require('./helpers/updateDB');
+updateLocaldb();
 bot.command('random', handleRandom);
 bot.on('callback_query', handleQuery);
 bot.on('message', handleAdd);
