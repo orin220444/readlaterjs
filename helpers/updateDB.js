7ef25f007a669318bd6a0da61320ed64b83ea2ad
Orin Serimon <orin220444@protonmail.com>
2020-05-12 16:53:29 +0700

    add updateDB.js

---
+++ helpers/updateDB.js
@@ -0,0 +1,11 @@
+const Post = require('../database/models');
+const fs = require('fs');
+/**
+ * saves local cache of the db to disk
+ */
+async function updateDb() {
+  let posts = await Post.find();
+  posts = JSON.stringify(posts);
+  fs.writeFileSync('database.json', String(posts));
+}
+module.exports = updateDb;
