cb03a3eafe199e150ed4e631eff74992921a85ab
Orin Serimon <orin220444@protonmail.com>
2020-05-12 18:32:39 +0700

    fix formatting

--- handlers/mainten.js
+++ handlers/mainten.js
@@ -7,36 +7,39 @@ module.exports = async () => {
     const post = posts[i];
     // console.log(post);
     console.log(`checking for duplicates for post ${post.originalURL}`);
-try {    await checkForDuplicates(post);
-}catch (error){
-console.log(error)
-}  
-}
+    try {
+      await checkForDuplicates(post);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 /**
  * checks for the duplicates because of shotten urls and redirect urls
  * @param {object} post post to check
  */
 async function checkForDuplicates(post) {
-const realUrl = setTimeout(async(post) => { const realurl = await getRealUrl(post.originalURL);
-return realurl
-  }, 15*1000, post)
-if (realUrl !== undefined) {
+  const realUrl = setTimeout(async (post) => {
+    const realurl = await getRealUrl(post.originalURL);
+    return realurl;
+  }, 15*1000, post);
+  if (realUrl !== undefined) {
     if (realUrl !== post.originalURL) {
       const original = Post.findOne({originalURL: realUrl});
       if (original !== undefined) {
         if (original === post) {
           console.log('finded a duplicate!');
-setTimeout(async(post) => {          await Post.findOneAndDelete({originalURL: post.originalURL});
-          }, 10* 1000, post)
-console.log('duplicate is deleted!');
+          setTimeout(async (post) => {
+            await Post.findOneAndDelete({originalURL: post.originalURL});
+          }, 10* 1000, post);
+          console.log('duplicate is deleted!');
         } else {
           console.log('updating url');
-setTimeout( async(post,realUrl) => {
-console.log(post)
-          post.originalURL = await realUrl;
-          await post.save();
-}, 20* 1000, post, realUrl)
+          setTimeout( async (post, realUrl) => {
+            console.log(post);
+            post.originalURL = await realUrl;
+            await post.save();
+          }, 20* 1000, post, realUrl);
         }
       }
     }
