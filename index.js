8e0eb935b39e46699061705f72ab4b208a93c0bb
Orin Serimon <orin220444@protonmail.com>
2020-05-12 18:13:24 +0700

    local cache now updates by the internal

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
 } = require('./handlers');
 const updateLocaldb = require('./helpers/updateDB');
 updateLocaldb();
+setInterval(() => updateLocaldb(), 900 *1000 );
 bot.command('random', handleRandom);
 bot.on('callback_query', handleQuery);
 bot.on('message', handleAdd);
