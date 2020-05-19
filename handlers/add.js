a9962609abbc72ce4827274144a695ee294836f0
Orin Serimon <orin220444@protonmail.com>
2020-05-19 14:55:40 +0700

    use for instead of for in

--- handlers/add.js
+++ handlers/add.js
@@ -12,34 +12,31 @@ module.exports = async (ctx) => {
 //  const answer = await ctx.reply('ишу ссылки');
   try {
     await finder(ctx.message).then( async (urls) => {
-      let i = 0;
       if (urls !== 'no urls!') {
         //        ctx.deleteMessage(answer.message_id);
-        for (i in urls) {
-          if ({}.hasOwnProperty.call(urls, i)) {
-            const url = urls[i];
-            if (url !== 'message.chat.id') {
-              console.log(url);
-              try {
-                const x = Math.random()*15 + 1;
-                console.log(`x = ${x}`);
-                setTimeout( async (url) => {
-                  await axios(url).then(async (realURL) => {
-                    console.log('sending url to the db');
-                    await saveToDB(realURL);
-                  });
+        for (let i = 0; i < urls.length; i++) {
+          const url = urls[i];
+          if (url !== 'message.chat.id') {
+            console.log(url);
+            try {
+              const x = Math.random()*15 + 1;
+              console.log(`x = ${x}`);
+              setTimeout( async (url) => {
+                await axios(url).then(async (realURL) => {
+                  console.log('sending url to the db');
+                  await saveToDB(realURL);
+                });
 
-                  // ctx.telegram.editMessageText(
-                  // ctx.chat.id, answer.message_id)
-                  // 'отправляю ссылки на сервер');
-                }, x, url);
-              } catch (error) {
-                console.log(error);
-              }
+                // ctx.telegram.editMessageText(
+                // ctx.chat.id, answer.message_id)
+                // 'отправляю ссылки на сервер');
+              }, x, url);
+            } catch (error) {
+              console.log(error);
             }
           }
-        // parse(url);
         }
+        // parse(url);
       }
     });
   } catch (error) {
