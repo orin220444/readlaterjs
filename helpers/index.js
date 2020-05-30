6eecaab13662d22477cecd1b41eefe79be2010cd
Orin Serimon <orin2204444@protonmail.com>
2020-05-30 18:47:34 +0700

    minor bug fix

--- helpers/index.js
+++ helpers/index.js
@@ -1,8 +1,10 @@
 const axios = require('./axios.js');
 const finder = require('./linkfinder.js');
 const saveToDB = require('./saveToDB.js');
+const keyboard = require('./keyboard.js');
 module.exports = {
   finder,
   saveToDB,
   axios,
+  keyboard,
 };
