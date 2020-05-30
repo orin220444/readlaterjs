3653437103b047331abbca2e41b77f1bcdcffb16
Orin Serimon <orin2204444@protonmail.com>
2020-05-30 16:47:42 +0700

    add teletype parser

--- helpers/parse.js
+++ helpers/parse.js
@@ -8,9 +8,13 @@ async function parse(url, page) {
   try {
     console.log('test');
     const $ = cheerio.load(page);
-    console.log(typeof $);
-    const body = $('body').text();
-    console.log(body);
+    // console.log(typeof $);
+    //    const body = $('body').text();
+    // console.log(`body: ${body}`);
+    const title = $('h1.article__header_title').text();
+    const article = $('article.article__content, text').text();
+    console.log(title);
+    console.log(article);
   } catch (error) {
     console.log(error);
   }
