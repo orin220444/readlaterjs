2add9ad98ee16955838db89c580a58a824c22154
Orin Serimon <orin2204444@protonmail.com>
2020-06-02 15:50:14 +0700

    code refactoring

--- handlers/add.js
+++ handlers/add.js
@@ -5,33 +5,48 @@ const {
   keyboard,
 } = require('../helpers');
 module.exports = async (ctx) => {
-  try {
-    await finder(ctx.message, function(urls) {
-      ;
-      if (urls !== 'no urls!') {
-        for (let i = 0; i < urls.length; i++) {
-          const url = urls[i];
-          if (url !== 'message.chat.id') {
-            console.log(url);
-            try {
-              const x = Math.random()*15 + 1;
-              console.log(`x = ${x}`);
-              setTimeout( async (url) => {
-                await axios(url, async function(realURL) {
-                  console.log('sending url to the db');
-                  await saveToDB(realURL);
-                });
-              }, x, url);
-            } catch (error) {
-              console.log(error);
-            }
-          }
-          ctx.reply(url, keyboard,
-              {reply_to_message_id: ctx.message.message_id});
+  await finder(ctx.message, getUrl(urls));
+
+  /**
+* check urls array for errors
+* @param {array} urls array of parsed urls
+*/
+  async function getUrl(urls) {
+    if (urls !== 'no urls!') {
+      for (let i = 0; i < urls.length; i++) {
+        const url = urls[i];
+        if (url !== 'message.chat.id') {
+          console.log(url);
+          saveUrl(url);
         }
-      };
-    });
-  } catch (error) {
-    console.log(error);
+      }
+    }
   }
+
+  /**
+* saves url
+* @param {string} url url to save
+*/
+  async function saveUrl(url) {
+    try {
+      const x = Math.random()*15 + 1;
+      console.log(`x = ${x}`);
+      setTimeout( async (url) => {
+        await axios(url, async function(realURL) {
+          console.log('sending url to the db');
+          await saveToDB(realURL).then( logSuccess(realURL));
+        });
+      }, x, url);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  /**
+* logs to user if success saving url
+* @param {string} realURL url to log
+*/
+  function logSuccess(realURL) {
+    ctx.reply(realURL, keyboard,
+        {reply_to_message_id: ctx.message.message_id});
+  };
 };
