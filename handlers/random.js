43cdd5ee2a8068468ba8db51e6121bc0b8f523d8
Orin Serimon <orin2204444@protonmail.com>
2020-06-02 17:09:13 +0700

    code refactoring

--- handlers/random.js
+++ handlers/random.js
@@ -1,20 +1,16 @@
 const keyboard = require('../helpers/keyboard.js');
 module.exports = async (ctx) => {
-  const dataBase = require('../database.json');
-  const posts = nonReadedPosts(dataBase);
-  const randompost = posts[
-      Math.floor(Math.random()*posts.length)
-  ];
-  console.log(randompost.originalURL);
+  const randomPost = getPost();
+  console.log(`Random post: ${randomPost.originalURL}`);
   ctx.reply(
-      randompost.originalURL, keyboard,
+      randomPost.originalURL, keyboard,
       {reply_to_message_id: ctx.message.message_id});
   /**
-   * filters non readed posts
+   * filters non read posts
    * @param {object} data data from database
-   * @return {object} non readed posts
+   * @return {object} non read posts
    */
-  function nonReadedPosts(data) {
+  function nonReadPosts(data) {
     const posts = [];
     for (let i = 0; i < data.length; i++) {
       const post = data[i];
@@ -26,5 +22,17 @@ module.exports = async (ctx) => {
       }
     }
     return posts;
+  }
+  /**
+   * send random non read post to user
+   * @return {object} post from the db
+   */
+  function getPost() {
+    const dataBase = require('../database.json');
+    const posts = nonReadPosts(dataBase);
+    const randomPost = posts[
+        Math.floor(Math.random()*posts.length)
+    ];
+    return randomPost;
   }
 };
