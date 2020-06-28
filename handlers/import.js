25e0d7a91b2064563aa3a8cbf8781bdbad51561b
Orin Serimon <orin220444@protonmail.com>
2020-06-28 10:54:49 +0000

    minor fixes

--- handlers/import.js
+++ handlers/import.js
@@ -1,15 +1,13 @@
 const bot = require('../bot');
 const axios = require('axios');
-const fs =require('fs');
+const fs = require('fs');
 module.exports = async (ctx) => {
   ctx.reply('send a file');
   bot.on('message', async (ctx) => {
-    console.log(ctx.message);
     const file = await ctx.telegram.getFileLink(ctx.message.document.file_id);
     axios.get(file)
         .then(function(response) {
-          console.log(typeof response.data);
-          fs.writeFile(
+        fs.writeFile(
               `import${new Date().getSeconds()}.html`,
               response.data, function(err) {
                 if (err) console.log(`error while importing: ${err}`);
