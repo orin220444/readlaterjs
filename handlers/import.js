30b88fe5a96b69e800bb1e73c1fdb8ade11f4b98
Orin Serimon <orin220444@protonmail.com>
2020-06-28 11:16:48 +0000

    WIP:parsing import file

--- handlers/import.js
+++ handlers/import.js
@@ -1,17 +1,26 @@
 const bot = require('../bot');
 const axios = require('axios');
 const fs = require('fs');
-module.exports = async (ctx) => {
+const html2Json = require('html2json')
+async function getFile(ctx){
   ctx.reply('send a file');
   bot.on('message', async (ctx) => {
     const file = await ctx.telegram.getFileLink(ctx.message.document.file_id);
     axios.get(file)
         .then(function(response) {
+exportPosts(response.data)/*
         fs.writeFile(
               `import${new Date().getSeconds()}.html`,
               response.data, function(err) {
                 if (err) console.log(`error while importing: ${err}`);
-              });
+else exportPosts()
+              });*/
         });
   });
 };
+
+function exportPosts(exportHTML){
+const export = html2json(exportHTML)
+console.log(export)
+}
+module.exports(getFile)
