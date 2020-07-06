1ee82a941f484ccf1a88fff70489c7e1eae84b01
Orin Serimon <orin2204444@protonmail.com>
2020-07-06 17:28:11 +0700

    refactor using callbacks

--- handlers/random.js
+++ handlers/random.js
@@ -1,12 +1,14 @@
 import keyboard from '../helpers/keyboard.js';
 import {sendLog} from '../src/log.js';
-import {getAllPosts} from '../database/index.js'
+import {getAllPosts} from '../database/index.js';
 export default async (ctx) => {
-  const randomPost = getPost();
-  sendLog(`Random post: ${randomPost.originalURL}`);
-  ctx.reply(
-      randomPost.originalURL, keyboard,
-      {reply_to_message_id: ctx.message.message_id});
+  getPost(function(randomPost) {
+    sendLog(`Random post: ${randomPost.originalURL}`);
+    ctx.reply(
+        randomPost.originalURL, keyboard,
+        {reply_to_message_id: ctx.message.message_id});
+  });
+
   /**
    * filters non read posts
    * @param {object} data data from database
@@ -29,11 +31,12 @@ export default async (ctx) => {
    * send random non read post to user
    * @return {object} post from the db
    */
-async  function getPost() {
-    getAllPosts(function(data){
-const posts = nonReadPosts(data)
-const randomPost = posts[Math.floor(Math.random() * posts.length)]
-return randomPost
-})
+  async function getPost(callback) {
+    getAllPosts(function(data) {
+      console.log(data);
+      const posts = nonReadPosts(data);
+      const randomPost = posts[Math.floor(Math.random() * posts.length)];
+      callback( randomPost);
+    });
   }
 };
