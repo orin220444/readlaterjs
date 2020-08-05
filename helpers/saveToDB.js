f8c8323ba2644bfd86ba82e7b4d5fd330f316454
Orin Serimon <orin220444@protonmail.com>
2020-08-05 14:44:28 +0700

    code refactoring using promises

--- helpers/saveToDB.js
+++ helpers/saveToDB.js
@@ -2,28 +2,41 @@ import {Post} from '../database/models.js';
 /**
 * saves url in the database
 * @param {any} url - url to save
+* @return {Promise} saved post
 */
-async function saveToDB(url) {
-  findDuplicates(url) .then(function(url) {
-    save(url);
+function saveToDB(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      findDuplicates(url) .then(function(url) {
+        save(url);
+        resolve();
+      });
+    } catch (error) {
+      reject(new Error(`error, not saving ${error}`));
+    }
   });
 }
 /**
  * save to db
  * @param {string} url url to save
+ * @return {Promise} saves url
  */
-async function save(url) {
-  try {
-    const post = await Post.create({
-      originalURL: url,
-    });
-    await post.save();
-    console.log(`${url} saved!`);
-  } catch (error) {
-    console.log(`error while saving to db: ${error}, ${url}`);
-  }
-}
+function save(url) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const post = await Post.create({
+        originalURL: url,
+      });
+      await post.save();
 
+      console.log(`${url} saved!`);
+      resolve();
+    } catch (error) {
+      reject(new Error(`error while saving to db: ${error}, ${url}`));
+    }
+  },
+  );
+}
 
 /**
  * checks for duplicates
