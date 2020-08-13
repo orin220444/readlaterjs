ef1e20ae15d725a33fd3af1aa5b27e91c920dae6
Orin Serimon <orin220444@protonmail.com>
2020-08-13 10:27:38 +0700

    add unarchive fun and trigger

--- handlers/query.js
+++ handlers/query.js
@@ -12,12 +12,27 @@ export default async (ctx) => {
     await deletePost(ctx.callbackQuery.message.text);
     ctx.reply('deleted',
         {reply_to_message_id: ctx.callbackQuery.message.message_id});
+  } else if (ctx.callbackQuery.data === 'Unarchive') {
+    await unArchive(ctx.callbackQuery.message.text);
+    sendLog('Unarchiving');
+    ctx.reply('Unarchived',
+        {reply_to_message_id: ctx.callbackQuery.message.message_id});
   }
   /**
    * set asRead = true in the db to the page
    * @param {string} url originalUrl of the web page
    */
   async function archive(url) {
+    const post = await Post.updateOne({originalURL: url},
+        {asReaded: true},
+    );
+    sendLog(`posts modified: ${post.nModified}`);
+  }
+  /**
+   * set asRead = false in the db to the page
+   * @param {string} url originalUrl of the web page
+   */
+  async function unArchive(url) {
     const post = await Post.updateOne({originalURL: url},
         {asReaded: true},
     );
