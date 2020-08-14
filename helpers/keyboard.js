f3c4dab8ab8073477a2cbfa77f3809b25174b114
Orin Serimon <orin220444@protonmail.com>
2020-08-14 11:51:03 +0700

    add archivKeyboard

--- helpers/keyboard.js
+++ helpers/keyboard.js
@@ -1,6 +1,10 @@
 import Markup from 'telegraf/markup.js';
-const keyboard = Markup.inlineKeyboard([
+const startKeyboard = Markup.inlineKeyboard([
   Markup.callbackButton('Archive', 'Readed'),
   Markup.callbackButton('Delete', 'Delete'),
 ]).extra();
-export default keyboard;
+const archiveKeyboard = Markup.inlineKeyboard([
+  Markup.callbackButton('Unarchive', 'Unarchive'),
+  Markup.callbackButton('Delete', 'Delete'),
+]).extra();
+export {startKeyboard, archiveKeyboard};
