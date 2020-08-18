b7541db11cdb2e523b879ee2d749ab4127026662
Orin Serimon <orin220444@protonmail.com>
2020-08-18 10:16:37 +0700

    off handleadd

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import 'dotenv/config.js';
 import {sendLog} from './src/index.js';
 import bot from './bot.js';
 import {
-  handleRandom, handleAdd, handleQuery, handleExport,
+  handleRandom, /* handleAdd,*/ handleQuery, handleExport, handleSearch,
 } from './handlers/index.js';
 bot.command('random', handleRandom);
 bot.command('export', handleExport);
 bot.on('callback_query', handleQuery);
-bot.on('message', handleAdd);
+// bot.on('message', handleAdd);
 
 bot.launch().then(
     sendLog('bot started'),
