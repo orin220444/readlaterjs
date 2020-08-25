946fda284e95d4ca037077c223a7a80543e17564
Orin Serimon <orin220444@protonmail.com>
2020-08-25 11:41:46 +0700

    init function telegraph

--- helpers/telegraph.js
+++ helpers/telegraph.js
@@ -1,8 +1,15 @@
-const Telegraph = require('telegraph-node');
+import Telegraph from 'telegraph-node';
 const ph = new Telegraph();
 ph.createAccount('save for later').then((result) => {
   console.log(result);
 });
-async function telegraph(url) {
+const token = process.env.TELEGRAPH_TOKEN;
+/**
+ * save post as a telegraph page
+ * @param {string} title
+ * @param {string} content
+ */
+export async function telegraph(title, content) {
+  ph.createPage(token, title, content).then((result) => console.log(result),
+  );
 }
-module.exports = telegraph;
