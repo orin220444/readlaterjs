d1d4cf19c0bde6138e7da8aa223f773a058d16a9
Orin Serimon <orin220444@protonmail.com>
2020-08-25 13:53:51 +0700

    refactor findOriginal

--- handlers/mainten.js
+++ handlers/mainten.js
@@ -21,23 +21,17 @@ export const handleMainten = async () => {
 async function checkForDuplicates(post, posts) {
   try {
     const realUrl = await urlCheck(post.originalURL);
-
-    if (realUrl !== undefined) {
-      if (realUrl !== post.originalURL) {
-      // TODO: combine checks
-        const original = findOriginal(post.originalURL);
-        if (original !== undefined) {
-          if (original) {
-            sendLog('finded a duplicate!');
-            await Post.findOneAndDelete({originalURL: post.originalURL});
-            sendLog('duplicate is deleted!');
-          } else {
-            sendLog('updating url');
-            sendLog(post);
-            post.originalURL = await realUrl;
-            await post.save();
-          }
-        }
+    if (realUrl !== post.originalURL) {
+      const isOriginal = findOriginal(post.originalURL);
+      if (isOriginal) {
+        sendLog('finded a duplicate!');
+        await Post.findOneAndDelete({originalURL: post.originalURL});
+        sendLog('duplicate is deleted!');
+      } else {
+        sendLog('updating url');
+        sendLog(post);
+        post.originalURL = await realUrl;
+        await post.save();
       }
     }
   } catch (error) {
@@ -49,14 +43,10 @@ async function checkForDuplicates(post, posts) {
    * @return {boolean} exists or not
    */
   function findOriginal(url) {
-    // TODO: refactor
-    for (let i = 0; i < posts.length; i++) {
-      const postURL = posts[i].originalURL;
-      if (postURL === url) {
-        return true;
-      } else {
-        return false;
-      }
+    for (const post of posts) {
+      const postURL = post.originalURL;
+      if (postURL === url) return true;
     }
+    return false;
   }
 }
