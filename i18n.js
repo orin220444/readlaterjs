aefb22d58de71c648fc4030c709b6903c08a6b02
Orin Serimon <orin220444@protonmail.com>
2020-08-25 11:47:02 +0700

    minor bug fixes

--- i18n.js
+++ i18n.js
@@ -1,8 +1,7 @@
 import I18n from 'telegraf-i18n';
 import path from 'path';
 export const i18n = new I18n({
-  directory: path.resolve(__dirname, '../locales'),
+  directory: path.resolve(__dirname, './locales'),
   defaultLanguage: 'ru',
   defaultLanguageOnMissing: true,
 });
-
