b3f250e8b759bfef6adc9ee24b9b04aa8b14df0d
Orin Serimon <orin220444@protonmail.com>
2020-08-25 14:22:47 +0700

    delete realUrl function

--- helpers/parse.js
+++ helpers/parse.js
@@ -1,31 +1,15 @@
 const Mercury = require('@postlight/mercury-parser');
-const axios = require('axios');
 /**
 * parses web page to text
 * @param {string} url url of web page to parse
 * @return {object} web page title and web page content
 */
 async function parse(url) {
-  const realUrl = await getRealURL(url);
-  const page = await Mercury.parse(realUrl, {contentType: 'text'})
+  const page = await Mercury.parse(url, {contentType: 'text'})
       .then((result) => {
         console.log(result.content);
         return {title: result.title, content: result.content};
       });
   return page;
-}
-/**
- * takes url and gets real url
- * @param {string} url - url to check
- * @return {string} real url to parse
- */
-async function getRealURL(url) {
-  const realURL = await axios.get(url)
-      .then(function(response) {
-        console.log(`Original url: ${url},
-real url ${response.request.res.responseUrl}`);
-        return response.request.res.responseUrl;
-      });
-  return realURL;
 }
 module.exports = parse;
