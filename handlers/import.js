cfdcde225aabcf5ec9b1454dd3d6ff8bd2c8a9af
Orin Serimon <orin220444@protonmail.com>
2020-08-27 14:39:48 +0700

    code refactoring

--- handlers/import.js
+++ handlers/import.js
@@ -1,20 +1,12 @@
-const bot = require('../bot');
-const axios = require('axios');
-const cheerio = require('cheerio');
-/**
- * get file from telegram
- * @param {any} ctx telegraf context
- */
-async function getFile(ctx) {
-  ctx.reply('send a file');
-  bot.on('message', async (ctx) => {
-    const file = await ctx.telegram.getFileLink(ctx.message.document.file_id);
-    axios.get(file)
-        .then(function(response) {
-          parseLinks(response.data);
-        });
-  });
-}
+import bot from '../bot';
+import axios from 'axios';
+import cheerio from 'cheerio';
+import {Post} from '../post';
+
+export const handleImport = async (ctx) => {
+  const data = await getFile(ctx);
+  saveLinks(parseLinks(data));
+};
 /**
  * parses links from pocket export file
  * @param {string} exportHTML HTML page
@@ -29,4 +21,29 @@ function parseLinks(exportHTML) {
   });
   return links;
 }
-module.exports = getFile;
+/**
+ * save posts in the db
+ * @param {Array} links - parsed links
+ */
+async function saveLinks(links) {
+  for (link of links) {
+    const post = new Post(link);
+    await post.savePost();
+  }
+}
+/**
+ * get file from telegram
+ * @param {any} ctx telegraf context
+ */
+async function getFile(ctx) {
+  ctx.reply('send a file');
+  const data = bot.on('message', async (ctx) => {
+    const file = await ctx.telegram.getFileLink(ctx.message.document.file_id);
+    const data = await axios.get(file)
+        .then(function(response) {
+          return response.data;
+        });
+    return data;
+  });
+  return data;
+}
