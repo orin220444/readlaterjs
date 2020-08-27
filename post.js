e7194b211b45088c6af1f1c6773c119e77fab67a
Orin Serimon <orin220444@protonmail.com>
2020-08-27 10:16:50 +0700

    move saveToDB to post

--- post.js
+++ post.js
@@ -1,5 +1,6 @@
 import {sendLog} from './src/log';
 import {axios} from 'axios';
+import {Post as PostModel} from './database/models.js';
 /** @typedef Post */
 export class Post {
   /** @param {string} url - url of the page */
@@ -26,7 +27,64 @@ export class Post {
           `axios error: ${error}, ${error.code}, ${error.config.url}`);
     }
   }
+  /**
+* saves url in the database
+* @param {any} url - url to save
+* @return {Promise} saved post
+*/
+  static saveToDB(url) {
+    return new Promise((resolve, reject) => {
+      try {
+        findDuplicates(url) .then(function(url) {
+          save(url);
+          resolve();
+        });
+      } catch (error) {
+        reject(new Error(`error, not saving ${error}`));
+      }
+    });
+  }
+  /**
+ * checks for duplicates
+ * @param {string} url url to save to db
+ *
+ * @return {Promise} url
+ */
+  static async findDuplicates(url) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const post = await PostModel.findOne({originalURL: url});
+        if (!post) {
+          resolve(url);
+        }
+      } catch (error) {
+        reject(
+            new Error(
+                `error while checking for duplicates: ${error}, ${url}`));
+      }
+    });
+  }
+  /**
+ * save to db
+ * @param {string} url url to save
+ * @return {Promise} saves url
+ */
+  static save(url) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const post = await PostModel.create({
+          originalURL: url,
+        });
+        await post.save();
 
+        console.log(`${url} saved!`);
+        resolve();
+      } catch (error) {
+        reject(new Error(`error while saving to db: ${error}, ${url}`));
+      }
+    },
+    );
+  }
 
   /**
 * saves url
