3661d28db4733505571aac1914af4c09d1bf90be
Orin Serimon <orin220444@protonmail.com>
2020-08-27 11:31:51 +0700

    minor bug fixes

--- handlers/export.js
+++ handlers/export.js
@@ -7,32 +7,33 @@ import {sendLog} from '../src/log.js';
 const createFile = promisify(writeFile);
 export const handleExport = async (ctx) => {
   console.log('getting posts to export');
-  const posts = await Post.find();
-  console.log('converting');
-  const options = {
-    fields: [
-      {
-        name: 'originalURL',
-        label: 'URL',
-        quoted: true,
-      },
-      {
-        name: 'asReaded',
-        label: 'Read',
-      },
-      {
-        name: 'created',
-        label: 'created',
-      },
-    ]};
-  buffered(posts, options)
-      .then((csv) => createFile('export.csv', csv))
-      .then(() => sendToUser())
-      .catch((error) => sendLog(error));
-
+  Post.find()
+      .then((posts) => {
+        console.log('converting');
+        const options = {
+          fields: [
+            {
+              name: 'originalURL',
+              label: 'URL',
+              quoted: true,
+            },
+            {
+              name: 'asReaded',
+              label: 'Read',
+            },
+            {
+              name: 'created',
+              label: 'created',
+            },
+          ]};
+        buffered(posts, options)
+            .then((csv) => createFile('export.csv', csv))
+            .then(() => sendToUser())
+            .catch((error) => sendLog(error));
       });
-};
-/**
+
+  /**
+   * send document to user
    */
   function sendToUser() {
     ctx.replyWithDocument({source: 'export.csv'},
