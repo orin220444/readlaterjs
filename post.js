023261f2c8b8f7126933739a4093083a7b69c0b4
Orin Serimon <orin220444@protonmail.com>
2020-08-27 10:06:24 +0700

    move urlCheck to post

--- post.js
+++ post.js
@@ -1,4 +1,5 @@
 import {sendLog} from './src/log';
+import {axios} from 'axios';
 /** @typedef Post */
 export class Post {
   /** @param {string} url - url of the page */
@@ -6,12 +7,34 @@ export class Post {
     this.url = url;
   }
   /**
+ * takes url and gets real url
+ * @param {string} url - url to check
+ *
+ * @return {Promise} real url
+ */
+  static async urlCheck(url) {
+    try {
+      this.realURL = await axios.get(url)
+          .then(function(response) {
+            const realURL = response.request.res.responseUrl;
+            sendLog(`Original url: ${url},
+  real url ${realURL}`);
+            return realURL;
+          });
+    } catch (error) {
+      throw new Error(
+          `axios error: ${error}, ${error.code}, ${error.config.url}`);
+    }
+  }
+
+
+  /**
 * saves url
 * @param {string} url url to save
 */
   async savePost() {
     try {
-      await urlCheck(this.url) .then(async function(realURL) {
+      await urlCheck(this.url) .then( async () => {
         sendLog('sending url to the db');
         await saveToDB(realURL);
       });
