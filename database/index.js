0262308b69fd3d768183d1a22c8706b273192d5a
Orin Serimon <orin220444@protonmail.com>
2020-09-05 12:51:12 +0700

    delete unused functions

--- database/index.js
+++ database/index.js
@@ -1,19 +1,4 @@
 import {Post} from './models.js';
-/** finds post in the db
- * @param {string} url of post to find
-*/
-async function search(url) {
-  const data = await Post.find(url, callback);
-  callback(data);
-}
-/**
- * get all posts from db
- * @param {callback} callback
- */
-async function getAllPosts(callback) {
-  const data = await Post.find();
-  callback(data);
-}
 /**
  * gets random post from the db
  * @return {object} random post
@@ -29,4 +14,4 @@ async function getRandomPost() {
 async function partialSearch(request) {
   return await Post.fuzzySearch(request);
 }
-export {search, getAllPosts, getRandomPost, partialSearch};
+export {getRandomPost, partialSearch};
