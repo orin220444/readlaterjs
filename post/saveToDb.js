be0f41beee06468bfab6e9fad647399b967ab607
Orin Serimon <orin220444@protonmail.com>
2020-09-10 19:39:30 +0700

    code ref

--- post/saveToDb.js
+++ post/saveToDb.js
@@ -1,21 +1,22 @@
 import {Post as PostModel} from '../database/models.js';
 /**
 * saves url in the database
-* @param {any} url - url to save
+* @param {string} url - url from user with redirects
+* @param {string} realUrl - url without redirects(may be broken)
+* @param {object} content - title + content
 * @return {Promise} saved post
 */
-export function saveToDB(url, realUrl, content /* pageUrl*/) {
-  return new Promise((resolve, reject) => {
-    try {
-      findDuplicates(url) .then(function(url) {
-        save(url, realUrl, content /* pageUrl*/);
-        resolve();
-      });
-    } catch (error) {
-      reject(new Error(`error, not saving ${error}`));
+export async function saveToDB(url, realUrl, content /* pageUrl*/) {
+  try {
+    const isDuples = await findDuplicates(url);
+    if (!isDuples) {
+      await save(url, realUrl, content /* pageUrl*/);
     }
-  });
+  } catch (error) {
+    throw new Error(`error, not saving ${error}`);
+  }
 }
+
 /**
    * checks for duplicates
    * @param {string} url url to save to db
@@ -23,42 +24,34 @@ export function saveToDB(url, realUrl, content /* pageUrl*/) {
    * @return {Promise} url
    */
 async function findDuplicates(url) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const post = await PostModel.findOne({originalUrl: url});
-      if (!post) {
-        resolve(url);
-      }
-    } catch (error) {
-      reject(
-          new Error(
-              `error while checking for duplicates: ${error}, ${url}`));
-    }
-  });
+  try {
+    const post = await PostModel.findOne({originalUrl: url});
+    return post ? true : false;
+  } catch (error) {
+    throw new Error(
+        `error while checking for duplicates: ${error}, ${url}`);
+  }
 }
 /**
    * save to db
    * @param {string} url - url from user with redirects
    * @param {string} realUrl - url without redirects(may be broken)
+   * @param {object} content - title + content
    * @return {Promise} saves url
    */
-export function save(url, realUrl, content /* telegraphUrl*/) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const post = await PostModel.create({
-        originalUrl: realUrl,
-        redirectUrl: url,
-        title: content.title,
-        content: content.content,
-        /* parsedUrl: telegraphUrl,*/
-      });
-      await post.save();
+export async function save(url, realUrl, content /* telegraphUrl*/) {
+  try {
+    const post = await PostModel.create({
+      originalUrl: realUrl,
+      redirectUrl: url,
+      title: content.title,
+      content: content.content,
+      /* parsedUrl: telegraphUrl,*/
+    });
+    await post.save();
 
-      console.log(`${url} saved!`);
-      resolve();
-    } catch (error) {
-      reject(new Error(`error while saving to db: ${error}, ${url}`));
-    }
-  },
-  );
+    console.log(`${url} saved!`);
+  } catch (error) {
+    reject(new Error(`error while saving to db: ${error}, ${url}`));
+  }
 }
