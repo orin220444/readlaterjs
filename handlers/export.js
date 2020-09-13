6d61576fb31fdcbd45d58657eccb05e1c4c53330
Orin Serimon <orin220444@protonmail.com>
2020-09-13 16:55:34 +0700

    export to html

--- handlers/export.js
+++ handlers/export.js
@@ -1,42 +1,35 @@
 import {Post} from '../database/models.js';
-import jsonCsv from 'json-csv';
+// import jsonCsv from 'json-csv';
 import {promisify} from 'util';
-const buffered = promisify(jsonCsv.buffered);
+import cheerio from 'cheerio';
+// const buffered = promisify(jsonCsv.buffered);
 import {writeFile} from 'fs';
 import {sendLog} from '../src/log.js';
 const createFile = promisify(writeFile);
 export const handleExport = async (ctx) => {
   console.log('getting posts to export');
-  Post.find()
-      .then((posts) => {
-        console.log('converting');
-        const options = {
-          fields: [
-            {
-              name: 'originalUrl',
-              label: 'URL',
-              quoted: true,
-            },
-            {
-              name: 'read',
-              label: 'Read',
-            },
-            {
-              name: 'created',
-              label: 'created',
-            },
-          ]};
-        buffered(posts, options)
-            .then((csv) => createFile('export.csv', csv))
-            .then(() => sendToUser())
-            .catch((error) => sendLog(error));
-      });
+  Post.find().then((posts)=> {
+    const $ = cheerio.load(
+        '<h1>Export from readlaterbot</h1>\n <ul id=links>\n');
+    posts.forEach(function(item) {
+      const post = item.toJSON();
+      // console.log(post.originalUrl ? post.originalUrl: post.originalURL)
+      const url = post.originalUrl ? post.originalUrl: post.originalURL;
+      $('#links').append(
+          `<li><a href="${url}">${url}</a></li>\n`,
+      );
+    });
 
-  /**
+    const html = $.html();
+    console.log(html);
+    createFile('export.html', html).then(() => sendToUser())
+        .catch((error) => sendLog(error));
+    /**
    * send document to user
    */
-  function sendToUser() {
-    ctx.replyWithDocument({source: 'export.csv'},
-    );
-  }
+    async function sendToUser() {
+      await ctx.replyWithDocument({source: 'export.html'},
+      );
+    }
+  });
 };
