1373785206277505dff1d1acb85468983b3b3fad
Orin Serimon <orin220444@protonmail.com>
2020-09-15 10:37:02 +0700

    fix semi

--- post/index.js
+++ post/index.js
@@ -11,7 +11,7 @@ export async function savePost(url) {
   try {
     const realUrl = await urlCheck(url);
     const parsedData = await parse(url);
-    await saveToDB(url, realUrl, parsedData)
+    await saveToDB(url, realUrl, parsedData);
   } catch (error) {
     sendLog(error);
   }
