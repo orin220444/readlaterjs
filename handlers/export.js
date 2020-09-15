85792336af60f46b93f800e7500ce3bff8eba1f1
Orin Serimon <orin220444@protonmail.com>
2020-09-15 10:24:19 +0700

    code ref. move exporting to html into a function

--- handlers/export.js
+++ handlers/export.js
@@ -9,33 +9,49 @@ const createFile = promisify(writeFile);
 export const handleExport = async (ctx) => {
   console.log('getting posts to export');
   Post.find().then((data)=> {
-    const posts = getPostsInJSON(data);
-    const $ = cheerio.load(
-        '<h1>Export from readlaterbot</h1>\n <ul id=links>\n');
-    posts.forEach(function(post) {
-      // console.log(post.originalUrl ? post.originalUrl: post.originalURL)
-      const url = post.originalUrl ? post.originalUrl: post.originalURL;
-      const title = post.title ? post.title : url;
-      $('#links').append(
-          `<li><a href="${url}">${title}</a></li>\n`,
-      );
-    });
-
-    const html = $.html();
-    console.log(html);
-    createFile('export.html', html).then(() => sendToUser())
+    const posts = getPostsInJson(data);
+    const exportData = exportToHtml(posts);
+    const fileExt = '.html';
+    createFile(`export${fileExt}`, exportData).then(() => sendToUser(fileExt))
         .catch((error) => sendLog(error));
     /**
    * send document to user
+   * @param {string} fileExt - extension of file to export
    */
-    async function sendToUser() {
-      await ctx.replyWithDocument({source: 'export.html'},
+    async function sendToUser(fileExt) {
+      await ctx.replyWithDocument({source: `export${fileExt}`},
       );
     }
   });
 };
-function getPostsInJSON(data) {
+/**
+ * because mongoose returns data in document format needs to convert to object
+ * @param {Array} data - mongoose documents
+ * @return {Array} - mongoose data in json
+ * */
+function getPostsInJson(data) {
   return data.map(function(item) {
     return item.toJSON();
   });
+}
+/**
+ * export data about posts in html
+ * @param {array} posts - mongoose data in json
+ * @return {string} - html document to export
+ */
+function exportToHtml(posts) {
+  const $ = cheerio.load(
+      '<h1>Export from readlaterbot</h1>\n <ul id=links>\n');
+  posts.forEach(function(post) {
+  // console.log(post.originalUrl ? post.originalUrl: post.originalURL)
+    const url = post.originalUrl ? post.originalUrl: post.originalURL;
+    const title = post.title ? post.title : url;
+    $('#links').append(
+        `<li><a href="${url}">${title}</a></li>\n`,
+    );
+  });
+
+  const html = $.html();
+  console.log(html);
+  return html;
 }
