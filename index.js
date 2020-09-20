917a7882bbdc14af5d4d21f4a13b3d2c5c9210ae
Orin Serimon <orin220444@protonmail.com>
2020-09-20 21:17:38 +0700

    code ref using stages

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ import {
   /* handleAdd,*/
   handleQuery, handleExport, handleSearch, handleImport, handleMainten,
 } from './handlers/index.js';
-const stage = new Stage([handleImport]);
+const stage = new Stage([handleImport, handleSearch]);
 
 bot.use(session());
 bot.use(stage.middleware());
 bot.command('random', handleRandom);
 bot.command('export', handleExport);
-bot.command('search', handleSearch);
+bot.command('search', (ctx) => ctx.scene.enter('SearchScene'));
 bot.command('import', (ctx) => ctx.scene.enter('ImportScene'));
 bot.command('mainten', handleMainten);
 bot.on('callback_query', handleQuery);
