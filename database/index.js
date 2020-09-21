c332ef7ed5291a16f9529adcd652dd43d6c8a331
Orin Serimon <orin220444@protonmail.com>
2020-09-21 10:33:20 +0700

    add getAllPosts, getPostsInJson

--- database/index.js
+++ database/index.js
@@ -22,4 +22,23 @@ async function partialSearch(request) {
     throw new Error(`error while searching: ${error}`);
   }
 }
-export {getRandomPost, partialSearch};
+/**
+ * gets all posts from db
+ */
+async function getAllPosts() {
+  const data = await Posts.find();
+  const posts = getPostsInJson(data);
+  return posts;
+}
+/**
+ * because mongoose returns data in document format needs to convert to object
+ * @param {Array} data - mongoose documents
+ * @return {Array} - mongoose data in json
+ * */
+function getPostsInJson(data) {
+  return data.map(function(item) {
+    return item.toJSON();
+  });
+}
+
+export {getRandomPost, partialSearch, getAllPosts};
