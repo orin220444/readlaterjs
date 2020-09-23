98a749dbd44501ee3ac97547021e2b9bb1e3af4c
Orin Serimon <orin220444@protonmail.com>
2020-09-23 21:35:33 +0700

    add ExportScene to scenes

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ import {
   /* handleAdd,*/
   handleQuery, handleExport, handleSearch, handleImport, handleMainten,
 } from './handlers/index.js';
-const stage = new Stage([handleImport, handleSearch]);
+const stage = new Stage([handleImport, handleSearch, handleExport]);
 
 bot.use(session());
 bot.use(stage.middleware());
 bot.command('random', handleRandom);
-bot.command('export', handleExport);
+bot.command('export', (ctx)=> ctx.scene.enter('ExportScene'));
 bot.command('search', (ctx) => ctx.scene.enter('SearchScene'));
 bot.command('import', (ctx) => ctx.scene.enter('ImportScene'));
 bot.command('mainten', handleMainten);
