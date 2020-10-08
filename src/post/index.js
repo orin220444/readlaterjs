3ff803a16bc1833a4a7221a0fca60dd35a5beb0d
Orin Serimon <orin220444@protonmail.com>
2020-10-08 20:57:49 +0700

    improve jsdoc using typedef

--- src/post/index.js
+++ src/post/index.js
@@ -9,12 +9,23 @@ import {urlCheck} from './urlCheck.js';
 */
 export async function savePost(url) {
   try {
-    const post = {};
-    post.url = url;
-    post.realUrl = await urlCheck(url);
     const parsedData = await parse(url);
-    post.title = await parsedData.title;
-    post.content = await parsedData.content;
+    /**
+     * Post data
+     * @public
+     * @typedef {Object} PostData
+     * @property {string} url - original url from user
+     * @property {string} realUrl - url after axios checking
+     * @property {string} title - title of the parsed page
+     * @property {string} content - html of the parsed page
+     */
+    /** @type {PostData} */
+    const post = {
+      url: url,
+      realUrl: await urlCheck(url),
+      title: parsedData.title,
+      content: parsedData.content,
+    };
     await saveToDB(post);
   } catch (error) {
     sendLog(error);
