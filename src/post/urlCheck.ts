108e4d7358175c699237fb82fac3e76e3b495cd4
Orin Serimon <orin220444@protonmail.com>
2020-10-10 18:55:03 +0700

    minor fix

--- src/post/urlCheck.ts
+++ src/post/urlCheck.ts
@@ -9,7 +9,7 @@ import {sendLog} from '../helpers/index.js';
 export async function urlCheck(url: string): Promise<string> {
   try {
     const data:any = await axios.get(url);
-        const realUrl:string = data.request.res.responseUrl;
+    const realUrl:string = data.request.res.responseUrl;
     return realUrl;
   } catch (error) {
     throw new Error(
