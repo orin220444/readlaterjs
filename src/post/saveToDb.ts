a8338e7082be37f51d45bbb49c972cb8dd43c2a3
Orin Serimon <orin220444@protonmail.com>
2020-10-12 18:35:37 +0700

    minor fixes

--- src/post/saveToDb.ts
+++ src/post/saveToDb.ts
@@ -1,10 +1,11 @@
 import {Post as PostModel} from '../database/models.js';
+import {PostData} from './index.js';
 /**
 * saves url in the database
 * @param {Object} postData
 * @return {Promise<void>} saved post
 */
-export async function saveToDB(postData: any) {
+export async function saveToDB(postData: PostData): Promise<void> {
   try {
     const isDuples = await findDuplicates(postData.url);
     if (!isDuples) {
@@ -21,7 +22,7 @@ export async function saveToDB(postData: any) {
    *
    * @return {Promise<boolean>} is url already in database
    */
-async function findDuplicates(url: any) {
+async function findDuplicates(url: string):Promise<boolean> {
   try {
     const post = await PostModel.findOne({originalUrl: url});
     return post ? true : false;
@@ -35,7 +36,7 @@ async function findDuplicates(url: any) {
    * @param {Object} postData
    * @return {Promise<void>} saves url
    */
-export async function save(postData: any) {
+export async function save(postData: PostData): Promise<void> {
   try {
     const post = await PostModel.create({
       originalUrl: postData.realUrl,
@@ -44,11 +45,8 @@ export async function save(postData: any) {
       content: postData.content,
     });
     await post.save();
-
-    // @ts-expect-error ts-migrate(2552) FIXME: Cannot find name 'url'. Did you mean 'URL'?
-    console.log(`${url} saved!`);
+    console.log(`${postData.url} saved!`);
   } catch (error) {
-    // @ts-expect-error ts-migrate(2552) FIXME: Cannot find name 'url'. Did you mean 'URL'?
-    throw new Error(`error while saving to db: ${error}, ${url}`);
+    throw new Error(`error while saving to db: ${error}, ${postData.url}`);
   }
 }
