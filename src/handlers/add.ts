46504f355f07e22f171fba46f57a01427475da7b
Orin Serimon <orin220444@protonmail.com>
2020-10-13 18:19:12 +0700

    code ref + fix typings

--- src/handlers/add.ts
+++ src/handlers/add.ts
@@ -1,20 +1,25 @@
 import {savePost} from '../post/index.js';
 import {finder, keyboard} from '../helpers/index.js';
 import {sendLog} from '../helpers/log.js';
-export const handleAdd = async (ctx: any) => {
-  getUrl(finder(ctx.message));
-  const message = await ctx.message.message_id;
+import {Context} from 'telegraf';
+export const handleAdd = async (ctx:Context):Promise<void> => {
+  const urls = finder(ctx.message);
+  await getUrl(urls);
+  const message = ctx.message?.message_id;
   /**
 * check urls array for errors
 * @param {array<string>} urls array of parsed urls
 */
-  async function getUrl(urls: string) {
+  async function getUrl(urls: string|Array<string>):Promise<void> {
     if (urls !== 'no urls!') {
       for (const url of urls) {
         if (url !== 'message.chat.id') {
-          // @ts-expect-error ts-migrate(2345) FIXME: Type 'Promise<void>' provides no match for the sig... Remove this comment to see the full error message
-          savePost(url).then(logSuccess(url))
-              .catch((error) => sendLog(error));
+          try {
+            await savePost(url);
+            await logSuccess(url);
+          } catch (error) {
+            sendLog(error);
+          }
         }
       }
     }
@@ -23,7 +28,7 @@ export const handleAdd = async (ctx: any) => {
 * logs to user if success saving url
 * @param {string} realUrl url to log
 */
-  async function logSuccess(realUrl: string) {
+  async function logSuccess(realUrl: string): Promise<void> {
     try {
       await ctx.reply(realUrl, keyboard,
           {reply_to_message_id: message});
