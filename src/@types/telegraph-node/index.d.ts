0c4eb659e94c62c77733dd640c8ae2897ccff03f
Orin Serimon <orin220444@protonmail.com>
2020-10-13 18:17:42 +0700

    declare telegraph-node

---
+++ src/@types/telegraph-node/index.d.ts
@@ -0,0 +1,9 @@
+declare module 'telegraph-node' {
+    export interface AccountInfo{
+        shortName: string,
+        authorName: string
+    }
+export class Telegraph {
+  createAccount(username:string, userInfo?:AccountInfo):Promise<undefined>
+}
+}
