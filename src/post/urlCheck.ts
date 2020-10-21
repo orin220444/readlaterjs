fd15d244f10f40057383cdf7e21627fcba77772f
Orin Serimon <orin220444@protonmail.com>
2020-10-21 18:31:57 +0700

    fix typings

--- src/post/urlCheck.ts
+++ src/post/urlCheck.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 import {sendLog} from '../helpers/index.js';
 /**
  * takes url and gets real url
@@ -6,9 +6,17 @@ import {sendLog} from '../helpers/index.js';
  *
  * @return {Promise} real url
  */
+interface AxiosData extends AxiosResponse {
+  data:string,
+  request: {
+    res: {
+      responseUrl:string
+    }
+  }
+}
 export async function urlCheck(url: string): Promise<string> {
   try {
-    const data = await axios.get(url);
+    const data:AxiosData = await axios.get(url);
     const realUrl:string = data.request.res.responseUrl;
     return realUrl;
   } catch (error) {
