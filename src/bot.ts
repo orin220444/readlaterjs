0805c2b77f7c29c3a858598eca1032b7bc94ca60
Orin Serimon <orin220444@protonmail.com>
2020-10-22 15:00:29 +0700

    minor fix

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
 import Telegraf from 'telegraf';
 const token = process.env.BOT_TOKEN;
 if (!token) {
-  throw new Error(`Token didn't exists`);
+  throw new Error(`Token doesn't exists`);
 }
 const bot = new Telegraf(token);
 export default bot;
